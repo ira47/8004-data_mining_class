dd1bea6fe2b24ed029c99bc6db62be970320d8ab
ira47 <1196951630@qq.com>
2020-04-28 11:11:35 +0800

    作业1 k-means框架搭建

--- 102-homework1/1.py
+++ 102-homework1/1.py
@@ -1,46 +1,49 @@
 import csv
-
-'''
-N_customer = 486
-N_category = 979
-'''
+import random
 
 
 class assignment1:
 
+    K = 10
+    N_CUSTOMER = 486
+    N_CATEGORY = 979
+
     '''
     成员变量注释：
-    1. category_max_sell：
-        含义：每个种类最多卖了多少。
-        计算方法：对属于各种类的订单的amt求和。
-        长度：N-category。
-        成员类型：float
-    2. jaccard：
-        成员为三元组[a,b,J-distance]。
-        变量含义：a和b是顾客的索引号index，满足a<b。J-distance是a b顾客间的Jaccard值。
-        长度：0.5*(N_customer)^2。
-        成员类型：[int,int,float]
-    3. customers_id：
+    1. customers_id：
         含义：记录顾客的ID编码。
-        长度：N_customer
+        长度：N_CUSTOMER
         成员类型：str
-    4. buy_dicts。
+    2. jaccard：
+        成员为字典[a,b]->J-distance。
+        变量含义：a和b是顾客的索引号index，满足a<b。J-distance是a b顾客间的Jaccard值。
+        长度：0.5*(N_CUSTOMER)^2。
+        成员类型：[int,int]->float
+    3. buy_dicts。
         含义：每一个顾客买了什么，以dict的形式展示。
-        长度：N_customer
+        长度：N_CUSTOMER
         成员类型：dict。key是种类的int形式表示，value是各种类的订单的amt求和。
-    5. category_index
-        含义：将类别序列化。输入种类，输出索引。
-        长度：N-category。
-        类型：dict。输入key的种类的int形式，输出种类对应的索引，以0开始。
-    6. category_total
-        含义：商品类型总计。在我们的样例中，值为979。
+    4. centroids
+        含义：每个簇的质心。
+        长度：K。
+        类型：dict。key是种类的int形式表示，value取属于该簇各点中，各种类的订单的amt求和的平均数。
+    5. K
+        含义：K-means中划分的簇的个数。
     '''
-    category_max_sell = []
-    jaccard = []
     customers_id = []
+    jaccard = {}
     buy_dicts = []
-    category_index = {}
-    category_total = 0
+    centroids = []
+
+    '''
+
+
+    以下是作业1-1代码。
+
+
+    '''
+
+    # 获得所有顾客的id
 
     def get_all_customers(self):
         with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
@@ -75,6 +78,14 @@ class assignment1:
                 print('用户ID：%s。它的词典：%s。' %
                       (self.customers_id[i], str(buy_dict)))
                 print()
+
+    '''
+
+
+    以下是作业1-2代码。
+
+
+    '''
 
     # 作业1-2 计算两个特定人之间的相似度
 
@@ -114,63 +125,45 @@ class assignment1:
             for b_index in range(len(self.customers_id)):
                 if a_index < b_index:
                     similarity = self.get_similarity(a_index, b_index)
-                    self.jaccard.append([a_index, b_index, similarity])
-
+                    self.jaccard[a_index, b_index] = similarity
                     count += 1
         print('-----------------------------------------')
-        print('已完成Jaccard系数计算，一共有%d个。以下输出一些示例。' % len(self.jaccard))
-        for i in range(len(self.jaccard)):
-            if i % 10000 == 0:
-                print(self.jaccard[i])
-    # 设计一个获得种类索引的函数
+        print('已完成Jaccard系数计算，一共有%d个。' % len(self.jaccard))
 
-    def update_category_index(self):
-        with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
-            lines = csv.reader(myFile)
-            for line in lines:
-                category = int(line[7][:5])
-                if category not in self.category_index.keys():
-                    self.category_index[category] = self.category_total
-                    self.category_total += 1
+    '''
+    
 
-        print('-----------------------------------------')
-        print('已完成种类索引，一共有%d个种类。' % self.category_total)
+    以下是作业1-3代码。
 
-    # 针对一个购买词典，生成其对应的向量，用来做k-means
-    def get_sample_vector(self, buy_dict):
-        vector = [0.0 for i in range(self.category_total)]
-        for key in buy_dict.keys():
-            index = self.category_index[key]
-            vector[index] = buy_dict[key]
-        return vector
 
-    # 获得所有的种类，并统计每个种类的amt总值
+    '''
 
-    def init_count_category(self):
-        with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
-            lines = csv.reader(myFile)
-            for line in lines:
-                category = int(line[7][:5])
-                self.count[category] += float(line[17])
+    def init_centroids(self):
+        customer_index_list = []
+        while len(customer_index_list) < K:
+            customer_index = int(
+                random.random()*self.N_CUSTOMER) % self.N_CUSTOMER
+            if customer_index not in customer_index_list:
+                self.centroids.apppend(buy_dicts[customer_index])
+                customer_index_list.append(customer_index)
 
-        total_category = 0
-        print('以下每隔100种，输出一个种类的信息。')
-        for i in range(100010):
-            if self.count[i] != 0.0:
-                total_category += 1
-                self.category_list.append(i)
-                if total_category % 100 == 0:
-                    print('第%d种品类：种类id：%d，总价：%.2f' %
-                          (total_category, i, self.count[i]))
+    def clustering(self):
 
-        print('一共有%d个种类。' % total_category)
+    def compare_cluster_result(self):
+
+    def next_generation(self):
+
+    def evaluate_cluster(self):
 
     def kmeans(self):
-        # 把用户的dict做成向量
-        buy_vectors = []
-        for buy_dict in self.buy_dicts:
-            vector = self.get_sample_vector(buy_dict)
-            buy_vectors.append(vector)
+        self.init_centroids()
+        while 1:
+            self.clustering()
+            is_equal_clustering_result = self.compare_cluster_result()
+            if is_equal_clustering_result:
+                break
+            self.next_generation()
+        self.evaluate_cluster()
 
 
 if __name__ == '__main__':
@@ -185,4 +178,4 @@ if __name__ == '__main__':
     ass1.count_jaccard()
     print('=========================================')
     print('作业1-3')
-    ass1.update_category_index()
+    ass1.kmeans()
