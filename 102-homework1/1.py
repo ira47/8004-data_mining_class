b7f4ed7d21f57d80bb63818a75c4c65042062e3e
ira47 <1196951630@qq.com>
2020-04-28 09:52:08 +0800

    作业1-1 尝试使用暴力法 解决cluster->failed

--- 102-homework1/1.py
+++ 102-homework1/1.py
@@ -2,70 +2,51 @@ import csv
 
 
 class assignment1:
-    count = [0.0 for i in range(100010)]
-    category_list = []
-    jaccard = []
+    category_max_sell = []
+    jaccard = []                    # 一个
     customers_id = []
     buy_dicts = []
+    category_index = {}
+    category_total = 0
 
-    # 作业1-1 获得所有的种类，并统计每个种类的amt总值
-
-    def init_count_category(self):
+    def get_all_customers(self):
         with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
             lines = csv.reader(myFile)
             for line in lines:
-                category = int(line[7][:5])
-                self.count[category] += float(line[17])
+                customer_id = line[5]
+                if customer_id not in self.customers_id:
+                    self.customers_id.append(customer_id)
+        print('-----------------------------------------')
+        print("加载所有的顾客id信息，一共有%d人。" % len(self.customers_id))
 
-        total_category = 0
-        print('以下每隔100种，输出一个种类的信息。')
-        for i in range(100010):
-            if self.count[i] != 0.0:
-                total_category += 1
-                self.category_list.append(i)
-                if total_category % 100 == 0:
-                    print('第%d种品类：种类id：%d，总价：%.2f' %
-                          (total_category, i, self.count[i]))
-
-        print('一共有%d个种类。' % total_category)
-
-    # 得到单个人的buy_list，效率过低，现已经弃用。
-
-    def get_buy_dict(self, a):
-        buy_dict = {}
-
-        with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
-            lines = csv.reader(myFile)
-            for line in lines:
-                if line[0] == a:
-                    category = int(line[7][:5])
-                    if category in buy_dict:
-                        buy_dict[category] += float(line[17])
-                    else:
-                        buy_dict[category] = float(line[17])
-
-        return buy_dict
-
-    # 批量更新buy_dict
+    # 作业1-1 批量更新buy_dict
 
     def update_buy_dicts(self):
         self.buy_dicts = [{} for i in range(len(self.customers_id))]
         with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
             lines = csv.reader(myFile)
             for line in lines:
-                index = self.customers_id.index(line[5])
+                customer_index = self.customers_id.index(line[5])
                 category = int(line[7][:5])
-                if category in self.buy_dicts[index].keys():
-                    self.buy_dicts[index][category] += float(line[17])
+                if category in self.buy_dicts[customer_index].keys():
+                    self.buy_dicts[customer_index][category] += float(line[17])
                 else:
-                    self.buy_dicts[index][category] = float(line[17])
+                    self.buy_dicts[customer_index][category] = float(line[17])
 
-        # return buy_dicts
+    def output_buy_dicts(self):
+        print('-----------------------------------------')
+        print("已按类别对用户进行商品金额汇总。下面显示一些数据。")
+        for i in range(len(self.buy_dicts)):
+            if i % 100 == 0:
+                buy_dict = self.buy_dicts[i]
+                print('用户ID：%s。它的词典：%s。' %
+                      (self.customers_id[i], str(buy_dict)))
+                print()
 
     # 作业1-2 计算两个特定人之间的相似度
 
     def get_similarity(self, a, b):
-        # 创建交集树和并集树
+        # 创建交集字典和并集字典
         union_dict = {}
         intersection_dict = {}
 
@@ -92,27 +73,15 @@ class assignment1:
             union_total += union_dict[key]
         return union_total/intersection_total
 
-    def get_all_customers(self):
-        with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
-            lines = csv.reader(myFile)
-            for line in lines:
-                customer_id = line[5]
-                if customer_id not in self.customers_id:
-                    self.customers_id.append(customer_id)
-        print('-----------------------------------------')
-        print("加载所有的顾客id信息，一共有%d人。" % len(self.customers_id))
-
     # 作业1-2 计算所有人的Jaccard系数
 
     def count_jaccard(self):
-        self.get_all_customers()
-        self.update_buy_dicts()
         count = 0
         for a_index in range(len(self.customers_id)):
             for b_index in range(len(self.customers_id)):
                 if a_index < b_index:
                     similarity = self.get_similarity(a_index, b_index)
-                    self.jaccard.append(, [a_index, b_index similarity])
+                    self.jaccard.append([a_index, b_index, similarity])
 
                     count += 1
         print('-----------------------------------------')
@@ -120,13 +89,46 @@ class assignment1:
         for i in range(len(self.jaccard)):
             if i % 10000 == 0:
                 print(self.jaccard[i])
+    # 设计一个获得种类索引的函数
+
+    def update_category_index(self):
+        with open('trade_new.csv', 'r', encoding='utf-8') as myFile:
+            lines = csv.reader(myFile)
+            for line in lines:
+                category = int(line[7][:5])
+                if category not in self.category_index.keys():
+                    self.category_index[category] = self.category_total
+                    self.category_total += 1
+
+        print('-----------------------------------------')
+        print('已完成种类索引，一共有%d个种类。' % self.category_total)
+
+    # 针对一个购买词典，生成其对应的向量，用来做k-means
+    def get_sample_vector(self, buy_dict):
+        vector = [0.0 for i in range(self.category_total)]
+        for key in buy_dict.keys():
+            index = self.category_index[key]
+            vector[index] = buy_dict[key]
+        return vector
+
+    def kmeans(self):
+        # 把用户的dict做成向量
+        buy_vectors = []
+        for buy_dict in self.buy_dicts:
+            vector = self.get_sample_vector(buy_dict)
+            buy_vectors.append(vector)
 
 
 if __name__ == '__main__':
     ass1 = assignment1()
     print('=========================================')
     print('作业1-1')
-    ass1.init_count_category()
+    ass1.get_all_customers()
+    ass1.update_buy_dicts()
+    ass1.output_buy_dicts()
     print('=========================================')
     print('作业1-2')
     ass1.count_jaccard()
+    print('=========================================')
+    print('作业1-3')
+    ass1.update_category_index()
